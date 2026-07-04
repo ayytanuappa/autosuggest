410842c6b03ebb77f12ecce333fd3341bde7d68e
B.Tanusree <banothtanusree@gmail.com>
2026-07-04 10:04:22 +0530

    changed the user

--- frontend/index.js
+++ frontend/index.js
@@ -1,25 +1,43 @@
 const users = [
   {
-    "name" :"Jane Doe",
+    "name" : "Jane Doe",
     "gender" : "Female",
-    "image" : "../images/jane.png"
+    "image" : "./images/jane.png"
   },
   {
-    "name" :"John Doe",
+    "name" : "John Doe",
     "gender" : "Male",
-    "image" : "../images/john.png"
-  },
-]
+    "image" : "./images/john.png"
+  }
+];
 let curUserId = 0;
 function toggle(){
+  console.log("frontend")
   if(curUserId == 0)
-    curUserId=1;
+    curUserId = 1;
   else
-    curUserId=0;
+    curUserId = 0;
   // image
-  document.getElementById("img").src=users[curUserId].image;
+  document.getElementById("img").src = users[curUserId].image;
   // name
-  document.getElementById("card-name").innerText=users[curUserId].name;
+  document.getElementById("card-name").innerText = users[curUserId].name;
+  // gender
+  document.getElementById("card-gender").innerText = users[curUserId].gender;
 }
-
-console.log("Hello from JS");
+function getRandomUser(){
+    fetch("https://randomuser.me/api/")
+        .then(response => {
+            return response.json();
+        })
+        .then(function(parsedData){
+            // console.log(parsedData);
+            let gender= parsedData.results[0].gender;
+            let first= parsedData.results[0].name.first;
+            let last= parsedData.results[0].name.last;
+            let name= first + " " + last;
+            let imageUrl= parsedData.results[0].picture.large;
+            document.getElementById("img").src = imageUrl;
+            document.getElementById("card-name").innerText = name;
+            document.getElementById("card-gender").innerText = gender;
+        });
+}
